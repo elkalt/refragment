914624740839110d27aba163065e7bd031ba112a
syranis <elliott.kalt@gmail.com>
2023-07-23 17:20:34 -0600

    small touchups for alpha release

--- src/lib/definitions/robots.ts
+++ src/lib/definitions/robots.ts
@@ -4,7 +4,7 @@ export let Robots: Map<string, Robot> = new Map<string, Robot>([
   [
     "Coal Miner",
     {
-      description: "Mine 1 coal every 5 seconds for 60 seconds",
+      description: "Build a miner with 10 energy and 5 iron to mine 1 coal every 5 seconds for 60 seconds",
       products: ["Coal"],
       baseProduction: [2],
       baseProductionInterval: 5,
