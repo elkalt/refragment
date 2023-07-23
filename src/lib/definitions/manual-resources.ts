106c347555447975d4e5d0f65958352b2c89f47f
syranis <elliott.kalt@gmail.com>
2023-07-22 22:55:04 -0600

    a possibility?

--- src/lib/definitions/manual-resources.ts
+++ src/lib/definitions/manual-resources.ts
@@ -2,4 +2,8 @@ import type { ManualResource } from '$lib/interfaces/manual-resource';
 
 export let ManualResources: Map<string, ManualResource> = new Map([
   ["Energy", {costIdentifier: "Time", baseCost: 10, baseProduction: 1}],
+  ["Circuits", {costIdentifier: "Time", baseCost: 10, baseProduction: 1}],
+  ["Circuits2", {costIdentifier: "Time", baseCost: 10, baseProduction: 1}],
+  ["Circuits3", {costIdentifier: "Time", baseCost: 10, baseProduction: 1}],
+  ["Circuits4", {costIdentifier: "Time", baseCost: 10, baseProduction: 1}],
 ]);
--- src/lib/definitions/resources.ts
+++ src/lib/definitions/resources.ts
@@ -2,5 +2,9 @@ import type { Resource } from "$lib/interfaces/resource";
 
 export let Resources: Map<string, Resource> = new Map([
   ["Energy", {unlocked: true, amount: 0}],
+  ["Circuits", {unlocked: true, amount: 0}],
+  ["Circuits2", {unlocked: true, amount: 0}],
+  ["Circuits3", {unlocked: true, amount: 0}],
+  ["Circuits4", {unlocked: true, amount: 0}],
   ["Research", {unlocked: false, amount: 0}],
 ]);
