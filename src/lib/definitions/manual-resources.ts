f207b1df59a54a5ab8a8fca4bd40fc82ec348d61
syranis <elliott.kalt@gmail.com>
2023-07-22 23:41:32 -0600

    move things around

--- src/lib/definitions/manual-resources.ts
+++ src/lib/definitions/manual-resources.ts
@@ -1,6 +1,22 @@
 import type { ManualResource } from '$lib/interfaces/manual-resource';
 
 export let ManualResources: Map<string, ManualResource> = new Map([
-  ["Generator", {products: ["Energy"], baseProduction: [1], consumed: ["Time"], baseCost: [5]}],
-  ["Mine", {products: ["Coal", "Iron Ore"], baseProduction: [3, 2], consumed: ["Energy"], baseCost: [5]}],
+  [
+    "Generator",
+    {
+      products: ["Energy"],
+      baseProduction: [1],
+      consumed: ["Time"],
+      baseCost: [5]
+    }
+  ],
+  [
+    "Mine",
+    {
+      products: ["Coal", "Iron Ore"],
+      baseProduction: [3, 2],
+      consumed: ["Energy"],
+      baseCost: [5]
+    }
+  ],
 ]);
