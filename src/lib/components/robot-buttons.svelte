50f88965ee7b6c00fc50f6cc98cf5c8ec29b4671
syranis <elliott.kalt@gmail.com>
2023-07-30 13:04:38 -0600

    new ButtonData interface

--- src/lib/components/robot-buttons.svelte
+++ src/lib/components/robot-buttons.svelte
@@ -1,24 +1,42 @@
 <script lang="ts">
   import '$lib/styles/app.scss'
   import type { Robot } from '$lib/interfaces/robot';
-  import type { Resource } from '$lib/interfaces/resource';
   import { RobotStore } from "$lib/stores/robot-store";
   import { ResourceStore } from '$lib/stores/resource-store';
   import Button from "./generic/rf-button.svelte";
+  import type { ButtonData } from '$lib/interfaces/button-data';
 
-  let resourceButtons: string[];
+  let resourceButtons: ButtonData[];
   $: resourceButtons = Array.from($RobotStore.entries())
     .filter(([k, v]) => {
       return v.unlocked;
     })
-    .map(([k, v]) => k);
+    .map(([k, v]) => {
+      return {
+        name: k,
+        inputs: mapInputs(v),
+        outputs: mapOutputs(v),
+        sustain: { interval: v.baseProductionInterval, totalTime: v.baseProductionTime },
+       };
+    });
   
   let resourceInputSatisfaction: Map<string, boolean> = new Map<string, boolean>();
-  $: resourceInputSatisfaction = new Map<string, boolean>(
-    Array.from($RobotStore.entries())
-      .filter(([k, v]) => v.unlocked)
-      .map(([k, v]) => [k, areInputsFufilled(v, $ResourceStore)])
-    );
+  $: {
+    for (let buttonData of resourceButtons) {
+      let button = $RobotStore.get(buttonData.name)!;
+      let satisfied = true;
+      for (let i = 0; i < button.inputs.length; i++) {
+        if (button.inputs[i] === "Time") {
+          continue;
+        }
+        if (button.baseCost[i] > $ResourceStore.get(button.inputs[i])!.amount) {
+          satisfied = false;
+          break;
+        }
+      }
+      resourceInputSatisfaction.set(buttonData.name, satisfied);
+    } 
+  }
 
   function mapInputs(resource: Robot): { input: string; amount: number }[] {
     let result: { input: string; amount: number }[] = [];
@@ -28,25 +46,10 @@
     return result;
   }
 
-  function mapProducts(resource: Robot): { output: string; amount: number }[] {
+  function mapOutputs(resource: Robot): { output: string; amount: number }[] {
     let result: { output: string; amount: number }[] = [];
     for (let i = 0; i < resource.products.length; i++) {
       result.push({ output: resource.products[i], amount: resource.baseProduction[i] });
-    }
-    return result;
-  }
-
-  function areInputsFufilled(resource: Robot, listener: Map<string, Resource>): boolean {
-    // this takes the resource store as an input so that the reactive declaration will update when it changes
-    let result = true;
-    for (let i = 0; i < resource.inputs.length; i++) {
-      if (resource.inputs[i] === "Time") {
-        continue;
-      }
-      if (resource.baseCost[i] > $ResourceStore.get(resource.inputs[i])!.amount) {
-        result = false;
-        break;
-      }
     }
     return result;
   }
@@ -54,17 +57,17 @@
 
 <h2>Robots</h2>
 <div class="button-container">
-  {#each resourceButtons as name}
-    {@const resourceData = $RobotStore.get(name)}
+  {#each resourceButtons as buttonData}
+  {@const resourceData = $RobotStore.get(buttonData.name)}
     {#if resourceData}
       <Button
-        on:click={() => RobotStore.use(name)}
-        name={name}
+        on:click={() => RobotStore.use(buttonData.name)}
+        name={buttonData.name}
         tooltip={resourceData.description}
-        inputs={mapInputs(resourceData)}
-        products={mapProducts(resourceData)}
-        disabled={resourceData.disabled || resourceInputSatisfaction.get(name)?.valueOf() === false}
-        sustain={{interval: resourceData.baseProductionInterval, totalTime: resourceData.baseProductionTime}}
+        inputs={buttonData.inputs}
+        products={buttonData.outputs}
+        disabled={resourceData.disabled || resourceInputSatisfaction.get(buttonData.name)?.valueOf() === false}
+        sustain={buttonData.sustain}
         />
     {/if}
   {/each}
