89e7a5802235504ddf71617610fcad7745ad3a63
elkalt <elliott.kalt@gmail.com>
2023-08-30 15:13:25 -0400

    structure manager buttons change on hover

--- src/lib/components/generic/rf-structure-manager.svelte
+++ src/lib/components/generic/rf-structure-manager.svelte
@@ -4,6 +4,11 @@
   export let active: number;
   export let max: number;
 
+  let oneHovered = false;
+  let tenHovered = false;
+  let minusOneHovered = false;
+  let minusTenHovered = false;
+
   let dispatch = createEventDispatcher();
 </script>
 
@@ -12,10 +17,38 @@
     Active: {active} / {max}
   </div>
   <div class="adjust-container">
-    <button on:click={() => dispatch("click", {amount: 1})}>+</button>
-    <button on:click={() => dispatch("click", {amount: 10})}>++</button>
-    <button on:click={() => dispatch("click", {amount: -1})}>-</button>
-    <button on:click={() => dispatch("click", {amount: -10})}>--</button>
+    <button
+      on:mouseenter={() => oneHovered = true}
+      on:focus={() => oneHovered = true}
+      on:mouseleave={() => oneHovered = false}
+      on:focusout={() => oneHovered = false}
+      on:click={() => dispatch("click", {amount: 1})}>
+      {oneHovered ? "+1" : "+"}
+    </button>
+    <button
+      on:mouseenter={() => tenHovered = true}
+      on:focus={() => tenHovered = true}
+      on:mouseleave={() => tenHovered = false}
+      on:focusout={() => tenHovered = false}
+      on:click={() => dispatch("click", {amount: 10})}>
+      {tenHovered ? "+10" : "++"}
+    </button>
+    <button
+      on:mouseenter={() => minusOneHovered = true}
+      on:focus={() => minusOneHovered = true}
+      on:mouseleave={() => minusOneHovered = false}
+      on:focusout={() => minusOneHovered = false}
+      on:click={() => dispatch("click", {amount: -1})}>
+      {minusOneHovered ? "-1" : "-"}
+    </button>
+    <button
+      on:mouseenter={() => minusTenHovered = true}
+      on:focus={() => minusTenHovered = true}
+      on:mouseleave={() => minusTenHovered = false}
+      on:focusout={() => minusTenHovered = false}
+      on:click={() => dispatch("click", {amount: -10})}>
+      {minusTenHovered ? "-10" : "--"}
+    </button>
   </div>
 </div>
 
@@ -32,10 +65,9 @@
     justify-content: space-between;
 
     .adjust-container {
-      align-items: center;
       display: grid;
-      grid-template-columns: repeat(2, 1fr);
-      grid-template-rows: repeat(2, 1fr);
+      grid-template-columns: repeat(2, 1.1rem);
+      grid-template-rows: repeat(2, 1.1rem);
       gap: 0.5rem;
 
       button {
